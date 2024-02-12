d6206a24b93050f050f6e20ba33709c0f88d0afc
Ankit Kumar <ak8415129@gmail.com>
2024-02-13 02:46:59 +0530

    start task 2

---
+++ Github Explorer/app.js
@@ -0,0 +1,10 @@
+const APIURL="https://api.github.com/users/";
+
+const getUser=async(username)=>{
+    const response= await fetch(APIURL+username);
+    const data=await response.json();
+    console.log(data);
+
+} 
+
+getUser("ak8415129");
